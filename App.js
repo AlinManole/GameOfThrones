358f4809985e52d6cfc510681aa4f5a075a6b6cd
Alin Manole <alin.manole@yahoo.com>
2021-12-02 13:44:59 +0100

    update finish

--- App.js
+++ App.js
@@ -1,63 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react'
-import { StyleSheet, Text, View, FlatList, Image, ScrollView, Modal } from 'react-native';
 
-export default function App() {
-  const [characters, setCharacters] = useState([]);
+import { Text, FlatList } from 'react-native'
+import { Modal, Provider } from '@ant-design/react-native'
+import Character from './components/Character'
+import Separator from './components/Separator'
+
+const App = () => {
+  const [characters, setCharacters] = useState([])
+  const [character, setCharacter] = useState(null)
 
   useEffect(() => {
     fetch("https://thronesapi.com/api/v2/Characters")
       .then(response => response.json())
       .then(data => setCharacters(data))
   }, [])
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Game Of Thrones</Text>
-      <ScrollView>
-        <Modal>
-          <FlatList horizontal={true} data={characters} renderItem={Character} />
-        </Modal>
-      </ScrollView>
-    </View>
-  );
-}
 
-const Character = ({ item }) => {
   return (
-    <View style={styles.character}>
-      <Text style={styles.name}>{item.fullName}</Text>
-      <Image
-        style={styles.image}
-        source={{
-          uri: item.imageUrl
-        }}
+    <Provider>
+      {character &&
+        <Modal
+          title="Title"
+          transparent
+          onClose={() => setCharacter(null)}
+          visible
+          closable
+        >
+          <Text>{character.fullName}</Text>
+          <Text>{character.title}</Text>
+          <Text>{character.family}</Text>
+        </Modal>
+      }
+      <FlatList
+        data={characters}
+        renderItem={({ item }) => (
+          <Character
+            character={item}
+            onPress={() => setCharacter(item) }
+          />
+        )}
+        ItemSeparatorComponent={Separator}
       />
-    </View>
+    </Provider>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 90,
-    padding: 40,
-  },
-  image: {
-    height: 250,
-    width: 250,
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  character: {
-    padding: 30,
-  },
-  name: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center'
-  }
-});
+export default App
